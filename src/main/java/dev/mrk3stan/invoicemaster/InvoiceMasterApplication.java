382f0f4afe49ccc3c660fe42f8e5eb4fa4889b8e
Mark Bustillo <markbustillo@cryptoblk.io>
2023-09-26 22:06:34 +0800

    generated codebase using spring initializer

---
+++ src/main/java/dev/mrk3stan/invoicemaster/InvoiceMasterApplication.java
@@ -0,0 +1,13 @@
+package dev.mrk3stan.invoicemaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mrk3stan/invoicemaster/InvoiceMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mrk3stan.invoicemaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
